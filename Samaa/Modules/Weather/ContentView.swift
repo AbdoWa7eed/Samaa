41f5eb7c1a95e3c5d8e753070626c36378fea340
AbdoWa7eed <abdo20132233@gmail.com>
2026-06-04 19:36:38 +0300

    chore: reorganize project folder structure

---
+++ Samaa/Modules/Weather/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherView.swift
+//  Samaa
+//
+//  Created by Abdelrahman on 04/06/2026.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("WELCOME TO SAMAA'!")
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
