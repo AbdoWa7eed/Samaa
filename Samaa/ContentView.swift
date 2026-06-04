5c52be4648db164af3e4c6f1f3535bc4f2a58a38
AbdoWa7eed <abdo20132233@gmail.com>
2026-06-04 19:14:33 +0300

    initial commit

---
+++ Samaa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Samaa
+//
+//  Created by Abdelrahman on 04/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Samaa/SamaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamaaApp.swift
+//  Samaa
+//
+//  Created by Abdelrahman on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SamaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
